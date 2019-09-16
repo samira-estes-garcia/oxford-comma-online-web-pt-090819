81fdbb6a0bb4eaa47a34bfecce740e03aa0d4cc5
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-16 19:25:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  elsif array.length == 2
+    array[-1].insert(0, " and ")
+    array.join
+  else
+    array.join
+  end
 end
