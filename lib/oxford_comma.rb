d294c11b70f396c60fcfcc67428f5005301b807c
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-16 19:11:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,5 @@
 def oxford_comma(array)
-  if array.length > 1
-    array.insert(-2, "and")
-    array.join(", ")
-  else 
-    array.join(", ")
-  end
+  new_array = []
+  array.each do |element|
+    
 end
